ca13a045372e9616c13113cd63f19f0c7054c8c2
Shahmeer Ahmed <ahmedshahmeer486@gmail.com>
2026-01-14 17:19:18 +0500

    main cleaned

--- backend/main.py
+++ backend/main.py
@@ -8,13 +8,12 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Frontend URLs
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
